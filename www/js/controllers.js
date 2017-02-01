4e88bba8606925bc8e62857abbc766817d32bacd
Claudia Caro <chilly2908@hotmail.com>
2017-02-01 14:59:49 +0200

    Add back button for ios.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,8 @@
 angular.module('starter.controllers', ['starter.services'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $ionicNavBarDelegate) {
+  var isIOS = ionic.Platform.isIOS();
+  $ionicNavBarDelegate.showBackButton(isIOS);
 
   // Form data for the login modal
   $scope.loginData = {};
